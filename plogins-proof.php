636771ed1a5719293266fd067fc7a2487a1cb876
wppoland <szatkowski.mariusz+github@gmail.com>
2026-07-28 09:30:07 +0200

    i18n: translate the PRO upgrade panel into Polish, German and Spanish

--- plogins-proof.php
+++ plogins-proof.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Proof - Social Proof for WooCommerce
  * Plugin URI:        https://plogins.com/plogins-proof/
  * Description:        Show recent-sale social-proof popups to build trust and urgency.
- * Version:           1.0.3
+ * Version:           1.0.4
  * Requires at least: 6.5
  * Tested up to:      7.0
  * Requires PHP:      8.1
@@ -26,7 +26,7 @@ namespace Proof;
 
 defined('ABSPATH') || exit;
 
-const VERSION     = '1.0.3';
+const VERSION     = '1.0.4';
 const PLUGIN_FILE = __FILE__;
 
 define('PROOF_DIR', plugin_dir_path(__FILE__));
